552be737664482a86065441fc204e555f31151a9
Anryan <2665572581@qq.com>
2017-11-29 17:33:37 +0800

    Update views.py
    what if i want to use for loop in the user dic

--- views.py
+++ views.py
@@ -4,6 +4,6 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-        user={'nickname':'anryan'}
+        user={'nickname':'anryan','nickname2':'syy','nickname3':'milkteawithpearlsin'}
         return render_template("index.html",title="home-anryan",user=user)
 	
