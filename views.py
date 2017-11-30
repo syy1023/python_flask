728bff53c2e2491a66605299f553b62a10511680
Anryan <2665572581@qq.com>
2017-11-30 10:00:22 +0800

    Update views.py
    add posts

--- views.py
+++ views.py
@@ -5,5 +5,11 @@ from flask import render_template
 @app.route('/index')
 def index():
         user={'nickname':'anryan','nickname2':'syy','nickname3':'milkteawithpearlsin'}
-        return render_template("index.html",title="home-anryan",user=user)
+	 posts=[
+                {'author':{'authorname':'michael'},
+                'body':'it is thursday today'},
+               {'author':{'authorname':'Jane'},'body':'it is a cloudy day'},
+               {'author':{'authorname':'Maria'},'body':'i do have bad luck today'}
+               ]
+        return render_template("index.html",title="home-anryan",user=user,posts=posts)
 	
